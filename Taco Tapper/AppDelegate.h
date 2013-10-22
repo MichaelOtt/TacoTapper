ed570cc940c81abff7158ef217af68d2c7cad120
Michael Ott <MichaelOtt@Michaels-MacBook-Pro.local>
2013-10-22 08:07:40 -0400

    Initial Commit

---
+++ Taco Tapper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Taco Tapper
+//
+//  Created by Michael Ott on 10/22/13.
+//  Copyright (c) 2013 Michael Ott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Taco Tapper/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Taco Tapper
+//
+//  Created by Michael Ott on 10/22/13.
+//  Copyright (c) 2013 Michael Ott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
